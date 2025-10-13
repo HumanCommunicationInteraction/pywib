e025e0d7b509bf0a2d232e573cfd2749b1d19c95
Guillermo Dylan Carvajal Aza <98907563+GuillermoDylan@users.noreply.github.com>
2025-10-13 13:37:28 +0200

    Add number of clicks and click slip

--- src/pywib/core/__init__.py
+++ src/pywib/core/__init__.py
@@ -5,6 +5,7 @@ from .timing import execution_time, movement_time, num_pauses, pauses_metrics
 from .movement import (velocity, acceleration, jerkiness, 
                        path, auc_optimal, auc_ratio, auc, 
                        velocity_metrics, acceleration_metrics, jerkiness_metrics)
+from .mouse import click_slip, number_of_clicks
 
 __all__ = [
     "execution_time",
